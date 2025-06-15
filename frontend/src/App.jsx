123eafc990dd5f89ed88c27ff10196bd1722cbc7
rolltheboll <ralphserranoastete@gmail.com>
2025-06-15 16:34:21 -0700

    Initial setup react + tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
